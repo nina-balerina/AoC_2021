e45e26133e60b2611932f2de16e9de6fbd99c3cc
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2021-12-07 11:56:33 +0100

    Update day 07

--- 07.py
+++ 07.py
@@ -8,13 +8,11 @@ for position in data[0].split(','):
 fuel_1 = [] 
 fuel_2 = []
 for i in range(max(positions)+1):
-    fuel_loc_1 = 0
-    fuel_loc_2 = 0
+    fuel_1.append(0)
+    fuel_2.append(0)
     for position in positions:
-        fuel_loc_1 = fuel_loc_1 + abs(i-position)
-        fuel_loc_2 = fuel_loc_2 + (abs(i-position)*(abs(i-position)+1))/2
-    fuel_1.append(fuel_loc_1)
-    fuel_2.append(int(fuel_loc_2))
+        fuel_1[i] = fuel_1[i] + abs(i-position)
+        fuel_2[i] = fuel_2[i] + (abs(i-position)*(abs(i-position)+1))//2
     
 print(min(fuel_1))
 print(min(fuel_2))
