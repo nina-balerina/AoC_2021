86755820445bc480b3c69726042e84a6a7661d61
Nina Ceh <71320289+nina-balerina@users.noreply.github.com>
2021-12-14 11:57:56 +0100

    Day 14 update

--- 14.py
+++ 14.py
@@ -1,30 +1,42 @@
 f = open('14input.txt', 'r')
 txt = f.readlines()
 import copy as cp
+from collections import defaultdict as dd 
 
-polymer = txt[0].strip('\n')
+polymer = '#' + txt[0].strip('\n') + '#'
 pair_insertion_rules = {}
 for i, line in enumerate(txt[2:]):
     find, add = line.split(' -> ')
     pair_insertion_rules[find] = add.strip()
-  
-for i in range(1,11):
-    new_polymer = ''
-    for char1, char2 in zip(polymer[0:len(polymer)], polymer[1:]):
-        if new_polymer=='':
-            new_polymer = char1
-        for rule in pair_insertion_rules:
-            if rule == char1+char2:
-                new_polymer = new_polymer +  pair_insertion_rules[rule] + char2
-        if new_polymer[-1] != char2:
-            new_polymer += char2
-    polymer = cp.copy(new_polymer)
 
-character_frequency = {}
-for rule in pair_insertion_rules:
-    character_frequency[pair_insertion_rules[rule]] = polymer.count(pair_insertion_rules[rule])
+group_frequency = dd(int)
+for char1, char2 in zip(polymer[0:-1], polymer[1:]):
+    group_frequency[char1+char2] += 1
+    
+for i in range(1,41):
+    new_frequency = dd(int)
+    for group in group_frequency:
+        if group in pair_insertion_rules:
+            new_frequency[group[0] + pair_insertion_rules[group]] += group_frequency[group] 
+            new_frequency[pair_insertion_rules[group] + group[1]] += group_frequency[group] 
+        if group not in pair_insertion_rules:
+                new_frequency[group] = group_frequency[group]
+    group_frequency = cp.copy(new_frequency)
+    if i == 10:
+        part1 = group_frequency
+
+character_frequency = dd(int)
+for pos, (char1, char2) in enumerate(part1):
+    if pos > 0:
+        character_frequency[char1] += part1[char1 + char2]
     
 value_list = list(character_frequency.values())
-print(max(value_list) - min(value_list))
+print(max(value_list) - min(value_list))   
 
-        
+character_frequency = dd(int)
+for pos, (char1, char2) in enumerate(group_frequency):
+    if pos > 0:
+        character_frequency[char1] += group_frequency[char1 + char2]
+    
+value_list = list(character_frequency.values())
+print(max(value_list) - min(value_list))     
